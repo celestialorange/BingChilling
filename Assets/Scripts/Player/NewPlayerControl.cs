bda38950a101089b46b541dbba15434caf755002
Celestial Orange <wdporange@icloud.com>
2023-06-12 00:37:05 +0100

    v1.13
    Solved the problem of sometimes player cannot jump. (I think.)

--- Assets/Scripts/Player/NewPlayerControl.cs
+++ Assets/Scripts/Player/NewPlayerControl.cs
@@ -20,9 +20,16 @@ public class NewPlayerControl : MonoBehaviour
     public float jumpForce = 5f;
     public int maxJumpCount = 2;
 
+    public Transform groundCheck;
+    public LayerMask ground;
+
     private Rigidbody2D rb;
-   [SerializeField]   private bool isJumping;
+    [SerializeField] private bool isJumping;
     public int jumpCount;
+
+    public bool isGround, isJump;
+
+    bool jumpPressed;
 
     //Animations
     public Animator animator;
@@ -52,74 +59,28 @@ public class NewPlayerControl : MonoBehaviour
 
     void Update()
     {
-        if (!isPlayerTrapped && !isCarCrash && !isGameCleared)
-        {
-            MovePlayer();
-        }
+        //if (!isPlayerTrapped && !isCarCrash && !isGameCleared)
+        //{
+        //    MovePlayer();
+        //}
         if (Input.GetButtonDown("Fire1") && gameManager.IceCreamBulletCount >= 1 && CouldFireIceCreamBullets)
         {
             Fire();
+        }
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            jumpPressed = true;
         }
 
         if (gameManager.isBulletIceCreamCreated)
         {
             CouldFireIceCreamBullets = true;
         }
-    }
-
- 
-
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.contacts[0].normal.y > 0.7f)
-        {
-            isJumping = false;
-            jumpCount = maxJumpCount;
-        }
-
-    }
-
-    void Jump()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        isJumping = true;
-        animator.SetBool("IsPlayerJump", true);
-    }
-
-    void MovePlayer()
-    {
-        //Jump Controls
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed * speedScale, rb.velocity.y);
-        //Debug.Log(rb.velocity);
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (isJumping)
-            {
-                if (jumpCount > 0)
-                {
-                    Jump();
-                    jumpCount--;
-                    soundFXManager.PlayPlayerSound(SoundType.Jump);
-                }
-            }
-            else
-            {
-                Jump();
-                jumpCount--;
-                soundFXManager.PlayPlayerSound(SoundType.Jump);
-            }
-        }
-
-        //Fall Detection
         if (gameObject.transform.position.y <= fallAnchor.transform.position.y)
         {
             gameManager.CommonDead();
         }
 
-        //Animation Control
         if (Input.GetButton("Horizontal"))
         {
             animator.SetBool("IsPlayerRun", true);
@@ -157,11 +118,107 @@ public class NewPlayerControl : MonoBehaviour
             animator.SetBool("IsPlayerJump", false);
             animator.SetBool("IsPlayerJumpDown", false);
         }
+    }
+
+    void FixedUpdate()
+    {
+        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground);
+        if (!isPlayerTrapped && !isCarCrash && !isGameCleared)
+        {
+            GroundMovement();
+            Jump();
+        }
+    }
+
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            isJumping = false;
+            jumpCount = maxJumpCount;
+        }
 
     }
+
+    void GroundMovement()
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+
+        if (horizontalMove != 0)
+        {
+            transform.localScale = new Vector3(horizontalMove, 1, 1);
+        }
+    }
+
+    void Jump()
+    {
+        if (isGround)
+        {
+            jumpCount = 2;
+            isJump = false;
+        }
+        if (jumpPressed && isGround)
+        {
+            isJump = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+            soundFXManager.PlayPlayerSound(SoundType.Jump);
+        }
+        else if (jumpPressed && jumpCount > 0 && !isGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+            soundFXManager.PlayPlayerSound(SoundType.Jump);
+        }
+        //void Jump()
+        //{
+        //    rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        //    isJumping = true;
+        //    animator.SetBool("IsPlayerJump", true);
+        //}
+
+        //void MovePlayer()
+        //{
+        //    //Jump Controls
+        //    float moveInput = Input.GetAxis("Horizontal");
+        //    rb.velocity = new Vector2(moveInput * speed * speedScale, rb.velocity.y);
+        //    //Debug.Log(rb.velocity);
+
+        //    if (Input.GetButtonDown("Jump"))
+        //    {
+        //        if (isJumping)
+        //        {
+        //            if (jumpCount > 0)
+        //            {
+        //                Jump();
+        //                jumpCount--;
+        //                soundFXManager.PlayPlayerSound(SoundType.Jump);
+        //            }
+        //        }
+        //        else
+        //        {
+        //            Jump();
+        //            jumpCount--;
+        //            soundFXManager.PlayPlayerSound(SoundType.Jump);
+        //        }
+        //    }
+
+            //Fall Detection
+          
+
+            //Animation Control
+           
+
+     }
+    
     void Fire()
     {
-        Instantiate(IceCreamBullet, FirePos.position, Quaternion.Euler(0,0, transform.localScale.x * 90));
+        Instantiate(IceCreamBullet, FirePos.position, Quaternion.Euler(0, 0, transform.localScale.x * 90));
         gameManager.IceCreamBulletCount -= 1;
         soundFXManager.PlaySound(SoundType.IceCreamFire);
     }
